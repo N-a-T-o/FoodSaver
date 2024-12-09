d636e822431c8266ae057c28f9d068e840b93d3b
Nadezhda Todorova <ntodor358@gmail.com>
2024-12-09 20:12:37 +0200

    initial commit

---
+++ server/src/main/java/com/foodsaver/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.foodsaver.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
