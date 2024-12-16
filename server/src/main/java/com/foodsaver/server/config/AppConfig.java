9f7c3430b9ac22dffb5800a981e80d219e67e23a
TodorYanakiev <todoryanakiev395@gmail.com>
2024-12-16 21:16:37 +0200

    Add jwt configuration files. Add AppConfig

---
+++ server/src/main/java/com/foodsaver/server/config/AppConfig.java
@@ -0,0 +1,34 @@
+package com.foodsaver.server.config;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.foodsaver.server.authorization.JwtAuthenticationEntryPoint;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+
+import java.time.format.DateTimeFormatter;
+
+public class AppConfig {
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+    @Bean
+    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        Jackson2ObjectMapperBuilder builder =
+                new Jackson2ObjectMapperBuilder()
+                        .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                        .serializers(new LocalDateTimeSerializer(formatter))
+                        .serializationInclusion(JsonInclude.Include.NON_NULL);
+        return new MappingJackson2HttpMessageConverter(builder.build());
+    }
+    @Bean
+    public JwtAuthenticationEntryPoint myJwtAuthenticationEntryPoint() {
+        return new JwtAuthenticationEntryPoint();
+    }
+}
+
