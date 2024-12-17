fd5b14f59f37e08d62e45fcbe28179a97eb02463
TodorYanakiev <todoryanakiev395@gmail.com>
2024-12-17 08:41:39 +0200

    Add UserDTO, AuthenticationService, AuthenticationController

---
+++ server/src/main/java/com/foodsaver/server/dtos/UserDTO.java
@@ -0,0 +1,44 @@
+package com.foodsaver.server.dtos;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.foodsaver.server.enums.Role;
+import com.foodsaver.server.model.VerificationToken;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+    private Long id;
+
+    private String email;
+
+    private String username;
+
+    private String password;
+
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", shape = JsonFormat.Shape.STRING)
+    private LocalDateTime createdAt;
+
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", shape = JsonFormat.Shape.STRING)
+    private LocalDateTime deletedAt;
+
+    private boolean enabled = true;
+
+    private List<VerificationToken> verificationTokens;
+
+    private Role role;
+}
