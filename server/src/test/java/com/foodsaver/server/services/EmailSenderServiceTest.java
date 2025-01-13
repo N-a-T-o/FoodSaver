c2dcdefce2f7e8ad7e8c144b9e009320c3fe3e3c
TodorYanakiev <todoryanakiev395@gmail.com>
2025-01-13 16:08:21 +0200

    Add import

--- server/src/test/java/com/foodsaver/server/services/EmailSenderServiceTest.java
+++ server/src/test/java/com/foodsaver/server/services/EmailSenderServiceTest.java
@@ -1,5 +1,6 @@
 package com.foodsaver.server.services;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -9,7 +10,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import java.util.Properties;
-=
 import static org.mockito.Mockito.*;
 
 class EmailSenderServiceTest {
