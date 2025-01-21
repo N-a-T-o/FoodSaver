41508c14c53312ce4e7ab5b6505a8bbce1403673
TodorYanakiev <todoryanakiev395@gmail.com>
2025-01-21 09:13:23 +0200

    Remove unused imports

--- server/src/main/java/com/foodsaver/server/dtos/NutritionalValuesDTO.java
+++ server/src/main/java/com/foodsaver/server/dtos/NutritionalValuesDTO.java
@@ -1,14 +1,9 @@
 package com.foodsaver.server.dtos;
 
-import com.foodsaver.server.model.Product;
-import jakarta.persistence.OneToOne;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.validator.constraints.Length;
 
 @Data
 @Builder
